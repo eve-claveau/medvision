323654f0e0df5610bdb4f68f23f212ad70e218fe
Eve Claveau <eve.claveau@mail.mcgill.ca>
2026-01-18 09:49:03 -0500

    commiting changes to index

--- flask/app.py
+++ flask/app.py
@@ -93,5 +93,5 @@ def stream():
 
 if __name__ == "__main__":
     host = os.environ.get("HOST", "0.0.0.0")
-    port = int(os.environ.get("PORT", "8000"))
+    port = int(os.environ.get("PORT", "5000"))
     app.run(host=host, port=port, debug=True, threaded=True)
